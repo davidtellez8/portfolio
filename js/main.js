d1332b5b4158ce408cd6aea1498544af11cf1c4b
dts <dtellezsolano.95@gmail.com>
2022-09-15 16:48:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+(function($) {
+	// LOADER
+	setTimeout(function() { 
+		if($('#ftco-loader').length > 0) {
+			$('#ftco-loader').removeClass('show');
+		}
+	}, 1);
+
+	$(window).scroll(function(){
+		var $w = $(this),
+				st = $w.scrollTop(),
+				navbar = $('.ftco_navbar'),
+				sd = $('.js-scroll-wrap');
+
+		if (st > 150) {
+			if ( !navbar.hasClass('scrolled') ) {
+				navbar.addClass('scrolled');	
+			}
+		} 
+		if (st < 150) {
+			if ( navbar.hasClass('scrolled') ) {
+				navbar.removeClass('scrolled sleep');
+			}
+		} 
+		if ( st > 350 ) {
+			if ( !navbar.hasClass('awake') ) {
+				navbar.addClass('awake');	
+			}
+			
+			if(sd.length > 0) {
+				sd.addClass('sleep');
+			}
+		}
+		if ( st < 350 ) {
+			if ( navbar.hasClass('awake') ) {
+				navbar.removeClass('awake');
+				navbar.addClass('sleep');
+			}
+			if(sd.length > 0) {
+				sd.removeClass('sleep');
+			}
+		}
+	});
+
+
+})(jQuery);
